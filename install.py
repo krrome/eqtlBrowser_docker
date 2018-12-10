3e9097664523b3fa9b9295b80097546243f102a3
RK <roman@dhcp-10-248-199-9.eduroam.wireless.private.cam.ac.uk>
2018-12-10 15:55:11 +0000

    include DB files in the image

--- install.py
+++ install.py
@@ -10,33 +10,15 @@ if __name__ == "__main__":
 
 	static_folder = os.path.join(wd, "static")
 	mongo_folder = os.path.join(wd, "mongodb_new_data")
-	static_zip = static_folder + ".zip"
-	mongo_zip = mongo_folder + "mongodb_new_data_2018_12_05.zip"
 
 	if not os.path.exists(static_zip):
 		os.system("python google_drive.py 1IHNAQDnLS-QwbINJtnxxR0_azAFzNAzL "+static_zip)
 		if os.path.exists(static_folder):
 			os.unlink(static_folder)
 		os.system("unzip {0} -d {1}".format(static_zip, cut_last_folder(static_folder)))
-
-	if not os.path.exists(mongo_zip):
-		os.system("python google_drive.py 1Jp0FwVb46RXVsbtcxHyzIzoEYSueIg67 "+mongo_zip)
-		if os.path.exists(mongo_folder):
-			os.unlink(mongo_folder)
-		os.system("unzip {0} -d {1}".format(mongo_zip, cut_last_folder(mongo_folder)))
-
 	
 	with open("docker-compose-template.yaml", "r") as ifh:
 		compose_template = yaml.load(ifh)
-
-	# set the mongodb volume link in the docker-compose file
-	vol_list_mongo = []
-	if 'volumes' in compose_template['services']['eqtl_mongodb']:
-		vol_list_mongo = compose_template['services']['eqtl_mongodb']['volumes']
-	else:
-		compose_template['services']['eqtl_mongodb']['volumes'] = vol_list_mongo
-
-	vol_list_mongo.append("{0}:{1}".format(mongo_folder, "/usr/share/mongo_data"))
 
 
 	# set the static volume link in the docker-compose file
